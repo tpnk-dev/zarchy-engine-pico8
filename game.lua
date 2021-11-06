8d93c9dc66136f927e8f6dc5da3543e837d116ba
tpnk_dev <marcospivetta60@gmail.com>
2021-11-06 20:48:20 -0300

    Main and basic movement

--- game.lua
+++ game.lua
@@ -1,6 +1,6 @@
 
 
-player = {x=80,y=0,z=80}
+player = {x=0,y=0,z=0}
 
 function game_init()
     main_update_draw = draw_update
@@ -9,23 +9,26 @@ end
 
 function logic_update()
     if(btn(0))then
-        player.x -= 5
+        player.x -= 2
     end
 
     if(btn(1))then
-        player.x += 5
+        player.x += 2
     end
 
     if(btn(2))then
-        player.z += 5
+        player.z += 2
     end
 
     if(btn(3))then
-        player.z -= 5
+        player.z -= 2
     end
 
-    cam_x = player.x 
-    cam_z = player.z - 80 - 80
+    player.x = player.x%(253*10)
+    
+
+    cam_x = player.x --+ 50 - 50
+    cam_z = player.z -70
     --cam_y = player.y
 end
 
