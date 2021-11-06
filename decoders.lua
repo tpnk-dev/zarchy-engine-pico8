d027333dedb7a6bd687094d42755c057584a4210
tpnk_dev <marcospivetta60@gmail.com>
2021-11-06 20:01:40 -0300

    Store objs in vertices

--- decoders.lua
+++ decoders.lua
@@ -1,4 +1,4 @@
--- spellcaster's base64 decoder | 194 TOKENS 
+-- spellcaster's base64 decoder and table storer | 279 TOKENS 
 base64str='0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_-+=[]}{;:<>,./?~|'
 
 function explode64(s)
@@ -30,30 +30,57 @@ function base64decode(str)
  return val
 end
 
-function store_rle_table(table, store)
-    local x,y,i,w=table[1]-1,0,3,table[1]
+function store_terrain_rle_table(table_heights, table_objs, multiplier)
+    local x,y,i,w=table_heights[1]-1,0,3,table_heights[1]
     local temp = {}
 
-    for s=0, table[1]-1 do
+    -- FReDs71 idea
+    -- 0x000f height info
+    -- 0x00f0 object info
+    -- 0x0f00 virus level
+
+    for s=0, table_heights[1]-1 do
         temp[s] = {}
     end
 
-    for i=#table,3,-1 do
-        local t=table[i]
-        local col,rle = (t& 0xff00)>>8
+    for i=#table_heights,3,-1 do
+        local t=table_heights[i]
+        local col,rle = (t& 0x0f00)>>8
                         ,t& 0xff
 
         for p=0, rle-1 do
-            temp[x-p][y] = {height=col}
+            temp[x-p][y] = col&0x000f
         end
         
         x-=rle
         
         if x < 0 then
-            x = table[1]-1
+            x = table_heights[1]-1
+            y += 1
+        end
+
+        --print(stat(0))
+    end
+
+    x,y,i,w=table_objs[1]-1,0,3,table_objs[1]
+
+    for i=#table_objs,3,-1 do
+        local t=table_objs[i]
+        local col,rle = (t& 0x0f00)>>4
+                        ,t& 0xff
+        
+        for p=0, rle-1 do
+            temp[x-p][y] = col&0x00f0 | temp[x-p][y]
+        end
+        
+        x-=rle
+        
+        if x < 0 then
+            x = table_objs[1]-1
             y += 1
         end
     end
 
+    
     return temp
 end
