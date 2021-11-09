659731fceb8daf861506697ca0e968783ed4ac19
tpnk_dev <marcospivetta60@gmail.com>
2021-11-09 18:58:16 -0300

    Truncate player position

--- game.lua
+++ game.lua
@@ -5,6 +5,9 @@ player = {x=0,y=0,z=0}
 function game_init()
     main_update_draw = draw_update
     main_update = logic_update
+
+    --add(game_objects3d, create_object3d(1, 2540, 0, 0))
+    add(game_objects3d, create_sprite3d(0, 0, 200,function(sprite) circfill(sprite.sx, sprite.sy, 0, 7) end))
 end
 
 function logic_update()
@@ -24,11 +27,13 @@ function logic_update()
         player.z -= 2
     end
 
-    player.x = player.x%(253*10)
-    
+    player.x = player.x%(256*tile_size)
+    player.z = player.z%(256*tile_size)
 
     cam_x = player.x --+ 50 - 50
-    cam_z = player.z -70
+    cam_z = player.z - 80
+
+    cam_y =  50
     --cam_y = player.y
 end
 
