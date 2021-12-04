cb770831bb96cf168f9f7554eafcc1d02a151e07
tpnk_dev <marcospivetta60@gmail.com>
2021-12-04 01:02:35 -0300

    update game to fit engine

--- game.lua
+++ game.lua
@@ -7,6 +7,11 @@ test_ax = 0
 -- these are the object ids in the scene, use return_model with the correct memory position to add new models
 OBJS_DATA = {decode_model(6685), decode_model(6730)}
 
+--COLORS
+pal(1, 140, 1)
+pal(13, 134,1)
+pal(15, 138,1)
+
 function game_init()
     init_engine()
 
@@ -17,13 +22,14 @@ function game_init()
 
     -- instantiate a sprite3d
     add(game_objects3d, create_sprite3d(player.x,player.y, player.z,
+                                        nil,nil,nil,
                                         function(sprite) local sx,sy=project_point(sprite.t_x,sprite.t_y,sprite.t_z) circfill(sx, sy, 1, 8) end,
                                         function(sprite) sprite.x,sprite.y,sprite.z = player.x,player.y,player.z end,
                                         function(sprite) end, 
-                                        nil, nil, nil))
+                                        nil, nil))
                         
     -- instantiate a object3d
-    add(game_objects3d, create_object3d(2, 0,100, 3,0,0,0,gravity))
+    add(game_objects3d, create_object3d(2, 0,100, 3,0,0,0,function(sprite) gravity(sprite, false, 3)  end))
 end
 
 function logic_update()
@@ -47,11 +53,11 @@ function logic_update()
     if(btn(4))then
         if(time()&0x0000.1000 == 0) then
             add(game_objects3d, create_sprite3d(player.x,player.y,player.z,
+                                    nil,nil,nil,
                                     function(sprite) local sx,sy=project_point(sprite.t_x,sprite.t_y,sprite.t_z) circfill(sx, sy, 0, sprite.life_span + 9) end,
-                                    function(sprite) gravity(sprite)  end,
-                                    function(sprite) srand(time()) sprite.y = player.y + 0.001 sprite.vy = 2 sprite.vx = rnd(0.4)-0.2 sprite.vz = rnd(0.4)-0.2 end, 
-                                    nil, nil, nil,
-                                    10))
+                                    function(sprite) gravity(sprite, false, 0.1)  end,
+                                    function(sprite) srand(time()) sprite.y = player.y + 0.001 sprite.vy = 2 sprite.vx = rnd(2)-1 sprite.vz = rnd(2)-1 end, 
+                                    10, true))
         end
 
     end
